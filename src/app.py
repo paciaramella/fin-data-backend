8202ddd6e6bbe1d68f13828808a42211309f4b98
Parker Ciaramella <43451736+paciaramella@users.noreply.github.com>
2024-07-11 13:01:29 -0400

    install CORS for frontend

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from apis.company_info import company_info_api
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all domains
 
 # Blueprints
 app.register_blueprint(company_info_api)
